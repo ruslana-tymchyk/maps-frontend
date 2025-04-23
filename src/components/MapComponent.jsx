488cdb23c60e18cac4a0315f7949e7f652e3cd41
ruslana-tymchyk <tymchyk3@gmail.com>
2025-04-23 07:33:03 +0100

    zoom fixed

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,13 +1,52 @@
-import { MapContainer, TileLayer, GeoJSON} from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, useMap} from 'react-leaflet';
+import { useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import geojsonData from '../sources/countries.json'; 
 
+const SetMapConstraints = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      // Set strict bounds
+      const bounds = [[-90, -180], [90, 180]];
+      
+      // Ensure the map stays within bounds and prevent wrapping
+      map.setMaxBounds(bounds)
+      
+      // Handler to force map back within bounds if it gets dragged outside
+      const enforceMapBounds = () => {
+        if (map) {
+          // panInsideBounds checks if the map's current view extends outside the specified bounds
+          map.panInsideBounds(bounds, { animate: false });
+        }
+      };
+      //Performing event based checking for bounds escape, as continous checking will affect performance
+      // Apply event listeners for various user interactions
+      map.on('drag', enforceMapBounds);
+      map.on('dragend', enforceMapBounds);
+      map.on('moveend', enforceMapBounds);
+      map.on('zoom', enforceMapBounds);
+      
+      return () => {
+        // Clean up event listeners when component unmounts
+        map.off('drag', enforceMapBounds);
+        map.off('dragend', enforceMapBounds);
+        map.off('moveend', enforceMapBounds);
+        map.off('zoom', enforceMapBounds);
+      };
+    }, [map]);
+    
+    return null;
+  };
 
 export default function MapComponent(){
+
 
     const highlightSelected = (e) => {
         // selected country
         const country = e.target;
+
+        // TODO: Make sure pop up shifts to the side if it crosses the bounds
 
         // Example: show a popup with feature info
         country.bindPopup(`<div>
@@ -55,21 +94,31 @@ export default function MapComponent(){
           // click: zoomToFeature
         })
       }
+
+
     return (
         <MapContainer 
-           center={[51.505, -0.09]} 
-           zoom={3} 
+           //attributes from map.options can be set here or in useEffect if need to be updated dynamically  
+           center={[40, 30]} 
+           zoom={2} 
            scrollWheelZoom={true} 
            style={{ height: '100%', width: '100%'}}
-           maxBounds={[[-90, -360], [90, 360]]}
+           noWrap={true}
+           minZoom={2}
+           maxZoom={8}
+           bounceAtZoomLimits={false}
            maxBoundsViscosity={1}>
+
+          <SetMapConstraints/> 
+
           <TileLayer
             attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
             url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}.png"
-            maxZoom='13'
+            minZoom={2}
+            maxZoom={8}
+            noWrap={true}
           />
-          {/* 'https://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}{r}.png' */}
-          {/* "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}" */}
+
           <GeoJSON attribution="&copy; geo-countries from Github.com" 
                    data={geojsonData} 
                    style={{fillColor: 'white',
